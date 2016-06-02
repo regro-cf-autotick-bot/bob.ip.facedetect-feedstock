60b2a2f276859631b6e4d9ba1f794f8f90381bb1
Amir Mohammadi <183.amir@gmail.com>
2016-06-02 10:29:20 +0200

    Add pthread.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 
 if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
+    export CFLAGS="${CFLAGS} -pthread"
+    export LDFLAGS="${LDFLAGS} -lpthread"
 fi
 
 $PYTHON -B setup.py install --single-version-externally-managed --record record.txt
